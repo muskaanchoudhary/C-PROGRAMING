7d6fef43991dbfc26a36ed9f5d6c23e34c3b42e7
Muskaan Choudhary <muskaanc007@gmail.com>
2024-10-27 16:41:50 +0530

    Update logical.c

--- logical.c
+++ logical.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2;
+    int a, b;
 
     // Input two numbers from the user
     printf("Enter the first number: ");
-    scanf("%d", &num1);
+    scanf("%d", &a);
 
     printf("Enter the second number: ");
-    scanf("%d", &num2);
+    scanf("%d", &b);
 
     // Using logical AND
-    if (num1 > 0 && num2 > 0) {
+    if (a > 0 && b > 0) {
         printf("Both numbers are positive.\n");
     } else {
         printf("At least one number is not positive.\n");
     }
 
     // Using logical OR
-    if (num1 < 0 || num2 < 0) {
+    if (a < 0 || b < 0) {
         printf("At least one number is negative.\n");
     } else {
         printf("Both numbers are non-negative.\n");
     }
 
     // Using logical NOT
-    if (!(num1 == 0)) {
+    if (!(a == 0)) {
         printf("The first number is not zero.\n");
     } else {
         printf("The first number is zero.\n");
